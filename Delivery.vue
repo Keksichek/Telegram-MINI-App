5cecfaed11f9a3e0fa8b9b8abf4e9e1656045f0b
Keksichek <166395757+Keksichek@users.noreply.github.com>
2026-05-03 16:09:48 +1000

    feat: Добавлена страница доставки

---
+++ Delivery.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref } from "vue";
+
+const address = ref("");
+const time = ref("");
+
+function submit() {
+  alert(`Заказ: ${address.value}, ${time.value}`);
+}
+</script>
+
+<template>
+  <div class="container">
+    <h1>🚚 Доставка</h1>
+
+    <input v-model="address" placeholder="Адрес" />
+    <br /><br />
+
+    <input v-model="time" placeholder="Время доставки" />
+    <br /><br />
+
+    <button @click="submit">
+      Далее
+    </button>
+  </div>
+</template>
