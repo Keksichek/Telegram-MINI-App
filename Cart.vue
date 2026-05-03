9f3872b559099fe12fca9cc20037b5b55a206c5a
Keksichek <166395757+Keksichek@users.noreply.github.com>
2026-05-03 16:08:56 +1000

    feat: Добавлена корзина товаров

---
+++ Cart.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { ref } from "vue";
+
+const cart = ref([]);
+</script>
+
+<template>
+  <div class="container">
+    <h1>🛒 Корзина</h1>
+
+    <p v-if="cart.length === 0">
+      Корзина пустая
+    </p>
+
+    <div v-for="item in cart" :key="item.id" class="card">
+      <h3>{{ item.name }}</h3>
+      <p>{{ item.price }} €</p>
+    </div>
+
+    <router-link to="/delivery">
+      Перейти к доставке
+    </router-link>
+  </div>
+</template>
